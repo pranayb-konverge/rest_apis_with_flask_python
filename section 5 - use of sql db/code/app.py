48ef5510a5297e7b2ce0839f52c1df4d6644c57e
Pranay Bankar <pranay.bankar@konverge.ai>
2022-03-16 12:34:54 +0530

    add a new file item to seperate the user and item table interaction and update app and create_table files accordingly

--- section 5 - use of sql db/code/app.py
+++ section 5 - use of sql db/code/app.py
@@ -1,63 +1,17 @@
-from flask import Flask, jsonify, request
-from flask_restful import Resource, Api, reqparse
+from flask import Flask
+from flask_restful import Api
 import uuid
-from flask_jwt import JWT, jwt_required
+from flask_jwt import JWT
 
 from security import authenticate, identity
 from user import UserRegister
+from item import Item, ItemsList
 
 app = Flask(__name__)
 app.secret_key = str(uuid.uuid4()) # make a random UUID
 api = Api(app)
 
 jwt = JWT(app, authenticate, identity) # /auth
-
-items = []
-
-class Item(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument("price",
-        type=float,
-        required=True,
-        help="This field cannot be blank!"
-    )
-    @jwt_required()
-    def get(self, name):
-        item = next(filter(lambda x: x["name"] == name, items), None)
-        return {"item": item}, 200 if item else 404
-
-    def post(self, name):
-        if next(filter(lambda x: x["name"] == name, items), None):
-            return {"message": f"An item with name '{name}' already exist."}, 400
-        
-        request_data = Item.parser.parse_args()
-        price = request_data["price"]
-        item = {"name":name, "price":price}
-        items.append(item)
-        return item, 201
-
-    def delete(self,name):
-        global items
-        items = list(filter(lambda x: x["name"] != name, items))
-        return {"message" : "Item Deleted!"}
-    
-    def put(self, name):        
-        data = Item.parser.parse_args()
-
-        # Once again, print something not in the args to verify everything works
-        item = next(filter(lambda x: x["name"] == name, items), None)
-        if item is None:
-            item = {"name" : name, "price": data["price"]}
-            items.append(item)
-        else:
-            item.update(data)
-        
-        return item
-
-class ItemsList(Resource):
-    def get(self):
-        return jsonify({"items":items})
-
 
 api.add_resource(Item, '/item/<string:name>') 
 api.add_resource(ItemsList, '/items') 
--- section 5 - use of sql db/code/create_tables.py
+++ section 5 - use of sql db/code/create_tables.py
@@ -3,8 +3,12 @@ import sqlite3
 connection = sqlite3.connect('data.db')
 cursor = connection.cursor()
 
-create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
-cursor.execute(create_table)
+# INTEGER is used for autoincrementation
+create_table_user= "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
+cursor.execute(create_table_user)
+
+create_table_item = "CREATE TABLE IF NOT EXISTS items (name text PRIMARY KEY, price real)"
+cursor.execute(create_table_item)
 
 connection.commit()
 connection.close()
